21140206e71314745c98298ffd802b27f79087bf
Zuhri Nurhuda <zuhri.nurhuda@bizzy.co.id>
2019-04-10 18:52:48 +0700

    components: create NavigationBar

---
+++ components/NavigationBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <nav class="uk-navbar-container" uk-navbar>
+    <div class="uk-navbar-left">
+      <ul class="uk-navbar-nav">
+        <li v-for="item in menu" :key="item.id">
+          <nuxt-link :to="item.path">{{ item.name }}</nuxt-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'navigation-bar',
+  data: function() {
+    return {
+      menu: [
+        { id: 1, name: 'Home', path: '/' },
+        { id: 2, name: 'Films', path: '/films' },
+        { id: 3, name: 'People', path: '/people' },
+        { id: 4, name: 'Locations', path: '/locations' },
+        { id: 5, name: 'Species', path: '/species' },
+        { id: 6, name: 'Vehicle', path: '/vehicle' }
+      ]
+    }
+  }
+}
+</script>
---
+++ plugins/uikit.js
@@ -0,0 +1,5 @@
+import UIkit from 'uikit'
+import Icons from 'uikit/dist/js/uikit-icons'
+
+// Load the Icon plugin
+UIkit.use(Icons)
